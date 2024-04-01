c7649589bcce546d60663adef598252a3ff6b51e
IdEatSomeFish <IdEatSomeFish@gmail.com>
2024-04-01 19:31:41 +0900

    added parameters to requests

--- src/p621/requests.py
+++ src/p621/requests.py
@@ -5,14 +5,26 @@ from requests import Response
 
 USER_AGENT: str = 'p621/0.0.0'
 
-def list_posts(api_key: str, username: str) -> list[Post]:
-    response: Response = requests.get('https://e621.net/posts.json', params = {'api_key': api_key, 'login': username}, headers = {'User-Agent': USER_AGENT})
+def list_posts(api_key: str, username: str, limit: int = None, tags: list[str] = None, page: int = None) -> list[Post]:
+    parameters: dict = {'api_key': api_key, 'login': username}
+    if limit:
+        parameters['limit'] = limit
+    if tags:
+        parameters['tags'] = ' '.join(tags)
+    if page:
+        parameters['page'] = page
+
+    response: Response = requests.get('https://e621.net/posts.json', params = parameters, headers = {'User-Agent': USER_AGENT})
     posts: dict = response.json()['posts']
 
     return [Post(post) for post in posts]
 
-def list_favorites(api_key: str, username: str) -> list[Post]:
-    response: Response = requests.get('https://e621.net/favorites.json', params = {'api_key': api_key, 'login': username}, headers = {'User-Agent': USER_AGENT})
+def list_favorites(api_key: str, username: str, user_id: int = None) -> list[Post]:
+    parameters: dict = {'api_key': api_key, 'login': username}
+    if user_id:
+        parameters['user_id'] = user_id
+
+    response: Response = requests.get('https://e621.net/favorites.json', params = parameters, headers = {'User-Agent': USER_AGENT})
     posts: dict = response.json()['posts']
 
     return [Post(post) for post in posts]
