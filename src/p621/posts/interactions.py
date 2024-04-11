462e6fdd9963d6f577e06bcc69c467fae85ac87b
IdEatSomeFish <IdEatSomeFish@gmail.com>
2024-04-12 00:14:58 +0900

    rewrote download_post using requests, creates directory if empty, skips download if post is url-less

--- src/p621/posts/interactions.py
+++ src/p621/posts/interactions.py
@@ -1,14 +1,24 @@
 from .classes import Post
 
 def download_post(post: Post, path: str = None) -> None:
-    import urllib.request
+    import requests
+    from requests import Response
+    import os
 
     target: str = str(post.id) + '.' + post.file.extension
     if path:
         target = path + '/' + target
 
+        if not os.path.exists(path):
+            os.mkdir(path)
+
     url: str = post.file.url
-    urllib.request.urlretrieve(url, target)
+
+    if url:
+        response: Response = requests.get(url)
+
+        with open(target, 'wb') as file:
+            file.write(response.content)
 
 def open_post(post: Post) -> None:
     import webbrowser
